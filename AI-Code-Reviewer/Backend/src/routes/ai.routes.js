3f2615166d845384097242bc149c64030fb0ba9b
Paras Rawat <rawatparas776@gmail.com>
2026-06-20 17:07:43 +0530

    react-router-dom

--- AI-Code-Reviewer/Backend/src/routes/ai.routes.js
+++ AI-Code-Reviewer/Backend/src/routes/ai.routes.js
@@ -4,5 +4,7 @@ const aiController = require("../controller/ai.controller");
 const router = express.Router();
 
 router.post("/get-review", aiController.getReview);
+router.get("/history", aiController.getHistory);
+router.delete("/history", aiController.clearHistory);
 
 module.exports = router;
